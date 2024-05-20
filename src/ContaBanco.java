e174e4a08de37b67e993aed2e1ab3001c4a3cc09
William Fontelas <williamdevsuporte@gmail.com>
2024-05-20 19:11:32 -0300

    desafio concluído

---
+++ src/ContaBanco.java
@@ -0,0 +1,12 @@
+public class ContaBanco {
+    
+    int numeroConta;
+
+    String agencia;
+
+    String nomeCliente;
+
+    String sobrenomeCliente;
+
+    double saldo;
+}
---
+++ src/ContaTerminal.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        
+        //criando o objeto Scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        //Instancando a classe ContaBanco
+        ContaBanco conta = new ContaBanco();
+
+        //Dados a serem inseridos
+        System.out.println("Por Favor, digite o número da Agência !");
+        conta.agencia = scanner.next();
+
+        System.out.println("Por Favor, digite o número da conta !");
+        conta.numeroConta = scanner.nextInt();
+
+        System.out.println("Por Favor, digite o seu nome !");
+        conta.nomeCliente = scanner.next();
+
+        System.out.println("Por Favor,digite o seu sobrenome !");
+        conta.sobrenomeCliente = scanner.next();
+
+        System.out.println("Por Favor, digite o valor inicial a ser depositado!");
+        conta.saldo =  scanner.nextDouble();
+
+        //Mensagem a ser exibida concatenado os dados inseridos
+        System.out.println("Olá " + conta.nomeCliente + " " + conta.sobrenomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + conta.agencia + ", conta " + conta.numeroConta + " e seu saldo de R$ " + conta.saldo + " já está disponível para saque." );   
+
+    }
+}
